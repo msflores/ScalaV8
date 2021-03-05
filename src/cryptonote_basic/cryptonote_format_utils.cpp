91cc816e8950880625731791a685698b3dad9da8
hayzamjs <hayzam@gmail.com>
2021-03-05 14:30:09 +0530

    Fix decimal point spec

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -975,13 +975,10 @@ namespace cryptonote
   {
     switch (decimal_point)
     {
-      case 12:
-      case 9:
-      case 6:
-      case 3:
+      case 2:
       case 0:
         default_decimal_point = decimal_point;
-        break;
+	break;
       default:
         ASSERT_MES_AND_THROW("Invalid decimal point specification: " << decimal_point);
     }
@@ -998,16 +995,10 @@ namespace cryptonote
       decimal_point = default_decimal_point;
     switch (decimal_point)
     {
-      case 12:
+      case 2:
         return "scala";
-      case 9:
-        return "millinero";
-      case 6:
-        return "micronero";
-      case 3:
-        return "nanonero";
       case 0:
-        return "piconero";
+        return "scent";
       default:
         ASSERT_MES_AND_THROW("Invalid decimal point specification: " << decimal_point);
     }
